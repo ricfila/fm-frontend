d3b3a5a92d47da23d1827eb92b1b85aab9fe528e
Riccardo Fila <riccardofila@gmail.com>
2025-09-28 00:26:57 +0200

    updated toast and modalConfirm functions

--- pannello/php/toast.php
+++ pannello/php/toast.php
@@ -13,7 +13,7 @@
 			<div class="modal-body" id="mcbody"></div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Annulla</button>
-				<button type="button" class="btn btn-success" id="mcok" onclick="" data-bs-dismiss="modal">OK</button>
+				<button type="button" class="btn btn-success" id="mcok">OK</button>
 			</div>
 		</div>
 	</div>
@@ -23,15 +23,15 @@
 var tok = 0;
 var tno = 0;
 
-function mostratoast(tipo, msg, tempo = false) {
-	if (tipo) {
-		$('#divtoast').append('<div class="toast bg-success text-white" id="tok' + (tok) + '" role="alert" style="border-radius: 10px; margin: 10px 0px 0px 0px;" data-bs-delay="' + (tempo == false ? '1500' : tempo * 1000) + '"><div class="d-flex">' +
+function showToast(type, msg, time = false) {
+	if (type) {
+		$('#divtoast').append('<div class="toast bg-success text-white" id="tok' + (tok) + '" role="alert" style="border-radius: 10px; margin: 10px 0px 0px 0px;" data-bs-delay="' + (time == false ? '1500' : time * 1000) + '"><div class="d-flex">' +
 		'<div class="toast-body">' + msg + '</div>' +
 		'<button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>');
 		$("#tok" + tok).toast("show");
 		tok++;
 	} else {
-		$('#divtoast').append('<div class="toast bg-danger text-white" id="tno' + (tno) + '" role="alert" style="border-radius: 10px; margin: 10px 0px 0px 0px;" data-bs-delay="' + (tempo == false ? '3000' : tempo * 1000) + '"><div class="d-flex">' +
+		$('#divtoast').append('<div class="toast bg-danger text-white" id="tno' + (tno) + '" role="alert" style="border-radius: 10px; margin: 10px 0px 0px 0px;" data-bs-delay="' + (time == false ? '3000' : time * 1000) + '"><div class="d-flex">' +
 		'<div class="toast-body">' + msg + '</div>' +
 		'<button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>');
 		$("#tno" + tno).toast("show");
@@ -41,10 +41,53 @@ function mostratoast(tipo, msg, tempo = false) {
 
 var modconfirm = new bootstrap.Modal(document.getElementById('modalconfirm'));
 
-function conferma(titolo, messaggio, azione) {
-	$('#mctitolo').html(titolo);
-	$('#mcbody').html(messaggio);
-	$('#mcok').attr('onclick', azione);
+var modalConfirmTransitioning = false;
+$('#modalconfirm').on('show.bs.modal', function() { modalConfirmTransitioning = true; });
+$('#modalconfirm').on('shown.bs.modal', function() { modalConfirmTransitioning = false; });
+$('#modalconfirm').on('hide.bs.modal', function() { modalConfirmTransitioning = true; });
+$('#modalconfirm').on('hidden.bs.modal', function() { modalConfirmTransitioning = false; });
+
+function modalConfirm(title, message) {
+	return new Promise(function(resolve) {
+		let resolved = false;
+
+		$('#mcok').off('click.confirm');
+		$('#modalconfirm').off('hide.bs.modal.confirm');
+		$('#modalconfirm .btn-danger').off('click.confirm');
+
+		$('#mcok').on('click.confirm', function() {
+			if (resolved) return;
+			resolved = true;
+			resolve(true);
+			modconfirm.hide();
+		});
+
+		$('#modalconfirm .btn-danger').on('click.confirm', function() {
+			if (resolved) return;
+			resolved = true;
+			resolve(false);
+			// modal will be hidden by data-bs-dismiss
+		});
+
+		$('#modalconfirm').on('hide.bs.modal.confirm', function() {
+			if (resolved) return;
+			resolved = true;
+			resolve(false);
+		});
+
+		if (modalConfirmTransitioning && $('#modalconfirm').hasClass('show') === false) {
+			$('#modalconfirm').one('hidden.bs.modal.waitShow', function() {
+				prepareModalConfirm(title, message);
+			});
+		} else {
+			prepareModalConfirm(title, message);
+		}
+	});
+}
+
+function prepareModalConfirm(title, message) {
+	$('#mctitolo').html(title);
+	$('#mcbody').html(message);
 	modconfirm.show();
 }
 </script>
